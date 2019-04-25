54f5701e7871fd2fcd6e2e01ecc72e6db3f46dd9
mmcgovern0 <mmcgovern.wework@gmail.com>
2019-04-25 14:14:21 -0400

    Updated Author/Post classes and Artist/Song classes

---
+++ lib/post.rb
@@ -0,0 +1,28 @@
+class Post
+
+	attr_accessor :author, :title
+
+	@@all = []
+
+	def initialize(title)
+		@title = title
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+
+	def title
+		@title
+	end
+
+	def author
+		@author
+	end
+
+	def author_name
+		self.author.name if author
+	end
+
+end
